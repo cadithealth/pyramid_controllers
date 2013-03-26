3ca5220a7087d9641e43432cf267775c05d0aa88
grabner <pjg.github@ubergrabner.net>
2013-03-26 00:12:41 -0400

    implemented multi-ext/name matching and added docs

--- pyramid_controllers/decorator.py
+++ pyramid_controllers/decorator.py
@@ -9,7 +9,7 @@
 #------------------------------------------------------------------------------
 
 import types, inspect
-from .util import adict
+from .util import adict, isstr
 
 #------------------------------------------------------------------------------
 class Decoration(object):
@@ -37,11 +37,16 @@ class Decorator(object):
 class ExposeDecorator(Decorator):
   attribute = 'expose'
   def enhance(self, wrapped, decoration, kw):
+    if kw.name and isstr(kw.name):
+      kw.name = [kw.name]
     if kw.ext:
       if kw.name:
         raise ValueError('@expose collision: "ext" and "name" are mutually exclusive')
       kw = adict(kw)
-      kw.name = wrapped.__name__ + '.' + kw.ext
+      if isstr(kw.ext):
+        kw.name = [wrapped.__name__ + '.' + kw.ext]
+      else:
+        kw.name = [wrapped.__name__ + '.' + e for e in kw.ext]
       del kw.ext
     super(ExposeDecorator, self).enhance(wrapped, decoration, kw)
 
@@ -52,16 +57,62 @@ class LookupDecorator(Decorator):  attribute = 'lookup'
 class DefaultDecorator(Decorator): attribute = 'default'
 
 #------------------------------------------------------------------------------
-def makeDecorator(klass):
+def makeDecorator(klass, doc=None):
   def decorator(*args, **kw):
     if len(args) == 1 and len(kw) == 0 and type(args[0]) == types.FunctionType:
       return klass()(args[0])
     return klass(*args, **kw)
+  decorator.__doc__ = doc
   return decorator
 
 #------------------------------------------------------------------------------
+expose = makeDecorator(ExposeDecorator, doc='''\
+
+  Decorates a :class:`Controller` method to indicate that it can be
+  invoked and traversed by the request dispatcher. Accepts the
+  following optional parameters:
+
+  :param renderer:
+
+    Specifies the renderer to use.
+
+  :param name:
+
+    Specifies the name or list of names that this method exposes for
+    dispatch. This is typically used when the external URL comprises
+    special characters or reserved words (such as ``def``). Note that
+    if this option is used, then the normal method name itself will no
+    longer be exposed unless explicitly listed.
+
+  :param ext:
+
+    Specifies the extension or list of extensions that this method
+    must be appended to for dispatch. This is typically used to
+    associate different renderers for different extensions. Note that
+    this option cannot be used in conjunction with `name` and that the
+    bare method name itself will no longer be exposed unless
+    explicitly listed.
+
+  Examples::
+
+    class MyController(Controller):
+
+      @expose
+      def action(self, request):
+        return 'reaction'
+
+      @expose(renderer='json')
+      def api(self, request):
+        return dict(state='enabled')
+
+      @expose(ext='html', renderer='module:path/to/template')
+      @expose(ext=('json', 'js'), renderer='json')
+      def resource(self, request):
+        return dict(count=12)
+
+  ''')
+
 # todo: add documentation for each decorator...
-expose  = makeDecorator(ExposeDecorator)
 index   = makeDecorator(IndexDecorator)
 lookup  = makeDecorator(LookupDecorator)
 default = makeDecorator(DefaultDecorator)
@@ -85,7 +136,10 @@ class ExposeDefaultsDecorator(object):
         for spec in getattr(apc, dectype, []):
           if 'ext' in self.kw:
             if 'name' not in spec and 'ext' not in spec:
-              spec.name = name + '.' + self.kw.ext
+              if isstr(self.kw.ext):
+                spec.name = [name + '.' + self.kw.ext]
+              else:
+                spec.name = [name + '.' + e for e in self.kw.ext]
     return wrapped
 
 #------------------------------------------------------------------------------
@@ -115,12 +169,12 @@ def expose_defaults(*args, **kw):
 
   Currently, the following parameters can be used in @expose_defaults:
 
-  * `renderer`:
+  :param renderer:
 
     any @index, @default, and @expose decorated methods that do not
     have a pre-existing `renderer` definition will inherit this value.
 
-  * `ext`:
+  :param ext:
 
     any @expose decorated methods that have neither a `name` or an
     `ext` definition will inherit this value.

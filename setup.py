8a7b5622cc3781e7576822148517f1674ce565a6
grabner <pjg.github@ubergrabner.net>
2013-03-28 12:54:40 -0400

    bumped version (for pypi)

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
 
 setup(
   name                  = 'pyramid_controllers',
-  version               = '0.3',
+  version               = '0.3.1',
   description           = 'A pyramid plugin that provides de-centralized hierarchical object dispatch.',
   long_description      = README,
   classifiers           = [
